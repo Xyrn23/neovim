650a848f775d5f216e8b7654f1319dea1d80aa8b
Xyrn23 <jerseyaaronpaulreyes@gmail.com>
2024-07-29 12:39:35 +0800

    changing something

--- lua/xkado/plugins/init.lua
+++ lua/xkado/plugins/init.lua
@@ -1,12 +1,4 @@
 return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
-  {
-    "NvChad/nvim-colorizer.lua",
-    opts = {
-      user_default_options = {
-        tailwind = true,
-      },
-    },
-  },
 }

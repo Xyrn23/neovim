303358ea8544883893fdf4fd0be19761d0c49f1d
Xyrn23 <jerseyaaronpaulreyes@gmail.com>
2025-02-17 09:23:35 +0800

    some changes

--- lua/xkado/plugins/nvim-colorizer.lua
+++ lua/xkado/plugins/nvim-colorizer.lua
@@ -1,30 +1,29 @@
 return {
-  -- -- Colorizer
-  -- {
-  --   'norcalli/nvim-colorizer.lua',
-
-  -- },
-  {
-      "catgoose/nvim-colorizer.lua",
-      event = "BufReadPre",
-      opts = { -- set to setup table
-      },
-      config = function()
-        require('colorizer').setup({
-          '*';
-        }, {
-          ARGB = true,
-          RGB      = true,   -- #RGB hex codes
-          RRGGBB   = true,   -- #RRGGBB hex codes
-          names    = true,   -- "Name" codes like Blue
-          RRGGBBAA = true,  -- #RRGGBBAA hex codes
-          rgb_fn   = true,  -- CSS rgb() and rgba() functions
-          hsl_fn   = true,  -- CSS hsl() and hsla() functions
-          css      = true,  -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-          css_fn   = true,  -- Enable all CSS *functions*: rgb_fn, hsl_fn
-          -- Available modes: foreground, background
-          mode     = 'backgroun' -- Set the display mode.
-        })
-      end
-  }
+    {
+        "NvChad/nvim-colorizer.lua",
+        opts = {
+            filetypes = { "*" },
+            user_default_options = {
+                RGB = true,          -- #RGB hex codes
+                RRGGBB = true,       -- #RRGGBB hex codes
+                names = true,        -- "Name" codes like Blue or blue
+                RRGGBBAA = true,     -- #RRGGBBAA hex codes
+                AARRGGBB = true,     -- 0xAARRGGBB hex codes
+                rgb_fn = true,       -- CSS rgb() and rgba() functions
+                hsl_fn = true,       -- CSS hsl() and hsla() functions
+                css = true,          -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+                css_fn = true,       -- Enable all CSS *functions*: rgb_fn, hsl_fn
+                -- Available modes for `mode`: foreground, background,  virtualtext
+                mode = "background", -- Set the display mode.
+                method = "both",
+                -- Available methods are false / true / "normal" / "lsp" / "both"
+                tailwind = true, -- Enable tailwind colors
+                sass = { enable = false, parsers = { "css" }, },
+                virtualtext = "■",
+                always_update = false
+            },
+            -- all the sub-options of filetypes apply to buftypes
+            buftypes = {},
+        }
+    }
 }

570aaa28487996b4ad27da119a4ecbc1bbde5e38
Xyrn23 <jerseyaaronpaulreyes@gmail.com>
2025-03-02 23:40:40 +0800

    refactoring lspconfig.lua

--- lua/xkado/plugins/lsp/mason.lua
+++ lua/xkado/plugins/lsp/mason.lua
@@ -5,14 +5,9 @@ return {
         "WhoIsSethDaniel/mason-tool-installer.nvim",
     },
     config = function()
-        -- import mason
         local mason = require("mason")
-
-        -- import mason-lspconfig
         local mason_lspconfig = require("mason-lspconfig")
-
         local mason_tool_installer = require("mason-tool-installer")
-        -- enable mason and configure icons
         mason.setup({
             ui = {
                 icons = {
@@ -34,6 +29,7 @@ return {
 
         mason_tool_installer.setup({
             ensure_installed = {
+                "prettierd"
             },
         })
     end,
